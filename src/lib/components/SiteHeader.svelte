3eb21059fdaee68caead187296f494eaa92115e3
n3-rd <godwinjdayo@gmail.com>
2025-04-29 10:15:39 +0100

    included the theme toggle to the new site header

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/state';
 	import { fly, fade } from 'svelte/transition';
 	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+	import Button from './ui/button/Button.svelte';
 
 	let showMobileMenu = false;
 	let searchQuery = '';
@@ -75,32 +76,37 @@
 				</kbd>
 			</div>
 
-			<!-- GitHub Icon -->
+		
+
+				
+				<ThemeToggle />
+
+					<!-- GitHub Icon -->
 			<a
-				href="https://github.com/ibrahimraimi/ui"
-				target="_blank"
-				rel="noreferrer"
-				class="hidden lg:flex rounded-md p-2 hover:bg-muted"
+			href="https://github.com/ibrahimraimi/ui"
+			target="_blank"
+			rel="noreferrer"
+			class="hidden lg:flex rounded-md p-2 hover:bg-muted"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="20"
+				height="20"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="h-5 w-5"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="20"
-					height="20"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="h-5 w-5"
-				>
-					<path
-						d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-					/>
-					<path d="M9 18c-4.51 2-5-2-7-2" />
-				</svg>
-				<span class="sr-only">GitHub</span>
-			</a>
+				<path
+					d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+				/>
+				<path d="M9 18c-4.51 2-5-2-7-2" />
+			</svg>
+			<span class="sr-only">GitHub</span>
+		</a>
 
 			<!-- Mobile Menu Button -->
 			<button
